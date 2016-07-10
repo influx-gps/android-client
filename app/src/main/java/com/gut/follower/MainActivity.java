e9439e3d88982cb04935e7ba32c786c5b08ee9d5
AdamGrzybkowski <agrzybkowski@outlook.com>
2016-07-10 20:42:21 +0200

    GUT-4 first app layout

---
+++ app/src/main/java/com/gut/follower/MainActivity.java
@@ -0,0 +1,49 @@
+package com.gut.follower;
+
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomBar bottomBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomBar = BottomBar.attach(this, savedInstanceState);
+        bottomBar.setItems(R.menu.three_buttons_menu);
+        bottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                switch(menuItemId){
+                    case R.id.record_item: break;
+                    case R.id.history_item: break;
+                    case R.id.profile_item: break;
+                }
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                switch(menuItemId){
+                    case R.id.record_item: break; //scroll to top after reselecting
+                    case R.id.history_item: break;
+                    case R.id.profile_item: break;
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        bottomBar.onSaveInstanceState(outState);
+    }
+}
